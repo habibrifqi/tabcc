2b3160edfda1a410d067d012b792c7f8f72e7155
Your Name <habib.rifqi.16@gmail.com>
2022-05-12 09:01:59 +0700

    isis halaman home sementara

--- home.php
+++ home.php
@@ -7,25 +7,12 @@
                 </h1>
             </div><!-- /.page-header -->
 
-            <div class="row">
+            <div class="row justify-content-center">
                 <div class="col-xs-12">
-                    <!-- PAGE CONTENT BEGINS -->
-
-
-                    <div class="row">
-                        <center>
-                            <h1>
-                                Implementasi Data Mining 
-                                <br>
-                                pada Penentuan Merk Parfum 
-                                yang banyak Terjual dengan Algoritma Apriori
-                            </h1>
-                            <p>
-                            <h2>
-                                (Studi Kasus: Copa Gabana Parfum Cabang Sentolang Gresik)
-                            </h2>
-                            </p>
-                        </center>
+                            <h1>APRIORI</h1>
+                      
+                            <h1>SKRIPSI</h1>
+                    </div>
 
                     </div><!-- /.row -->
 

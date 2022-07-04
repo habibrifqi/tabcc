3c2c5a4134135fdce031580f6bbc8b6d321debf6
Your Name <habib.rifqi.16@gmail.com>
2022-07-04 09:06:26 +0700

    memperbaiki nama menu

--- menu.php
+++ menu.php
@@ -8,7 +8,7 @@ if (isset($_GET['menu'])) {
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
       <img src="https://image.similarpng.com/very-thumbnail/2020/11/Coffee-logo-design-on-transparent-background-PNG.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">Temani NGopi Cafe</span>
+      <span class="brand-text font-weight-light">Temani Ngopi Cafe</span>
     </a>
     <!-- Sidebar -->
     <div class="sidebar">
